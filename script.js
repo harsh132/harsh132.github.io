b8e654e02f9bf002a52805b24b62038a6625fd3f
Harsh Prakash <prakashharsh32@gmail.com>
2021-03-01 01:32:16 +0530

    home section+nav of portfolio

---
+++ script.js
@@ -0,0 +1,14 @@
+// document.body.style.backgroundColor = "#0000ff";
+
+// setTimeout(() => (document.body.style.backgroundColor = "#ff0000"), 5000);
+
+VANTA.WAVES({
+	el: document.querySelector("#home"),
+	mouseControls: true,
+	touchControls: true,
+	gyroControls: false,
+	minHeight: 200.0,
+	minWidth: 200.0,
+	scale: 1.0,
+	scaleMobile: 1.0,
+});
