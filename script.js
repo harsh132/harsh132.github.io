45c78101f748bd110283b00284b626783b41c696
Harsh Prakash <prakashharsh32@gmail.com>
2021-03-01 18:15:16 +0530

    added projects section

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-// document.body.style.backgroundColor = "#0000ff";
-
-// setTimeout(() => (document.body.style.backgroundColor = "#ff0000"), 5000);
+const home = document.querySelector("#home");
+const nav = document.querySelector("nav");
+const navSec = document.getElementById("nav-sec");
+const menu = document.getElementById("menu");
 
 VANTA.WAVES({
 	el: document.querySelector("#home"),
@@ -9,6 +10,20 @@ VANTA.WAVES({
 	gyroControls: false,
 	minHeight: 200.0,
 	minWidth: 200.0,
-	scale: 1.0,
+	scale: 0.8,
 	scaleMobile: 1.0,
+});
+
+function toggle() {
+	navSec.classList.toggle("hidden");
+	menu.classList.toggle("icon-cancel");
+	menu.classList.toggle("icon-menu");
+}
+
+window.addEventListener("scroll", function () {
+	if (window.scrollY > 0.9 * home.clientHeight) {
+		nav.style.background = "#1596e3b0";
+	} else {
+		nav.style.background = "rgba(255, 255, 255, 0.3)";
+	}
 });
