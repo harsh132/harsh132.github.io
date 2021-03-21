40a450d97c52a1b2a980a9a5358a9aa6dcee763a
Harsh Prakash <prakashharsh32@gmail.com>
2021-03-21 17:12:40 +0530

    testing img lazy load

--- script.js
+++ script.js
@@ -15,3 +15,6 @@ window.addEventListener("scroll", function () {
 		nav.style.background = "rgba(255, 255, 255, 0.3)";
 	}
 });
+var imgs = document.querySelectorAll("img[data-src]").forEach(function (i) {
+	i.src = i.dataset.src;
+});
